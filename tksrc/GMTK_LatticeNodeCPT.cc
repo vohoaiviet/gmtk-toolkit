73111d35038bb8760497f24476cf974cab95f965
cvs2hg <cvs2hg@localhost>
2010-08-11 03:13:43 +0000

    fixup commit for tag 'pre_gmtktie_merge'

---
+++ tksrc/GMTK_LatticeNodeCPT.cc
@@ -0,0 +1,335 @@
+/*-
+ * GMTK_LatticeNodeCPT.cc
+ *
+ *  Written by Gang Ji <gang@ee.washington.edu>
+ * 
+ *  $Header$
+ * 
+ * Copyright (c) 2001, < fill in later >
+ *
+ * Permission to use, copy, modify, and distribute this
+ * software and its documentation for any non-commercial purpose
+ * and without fee is hereby granted, provided that the above copyright
+ * notice appears in all copies.  The University of Washington,
+ * Seattle make no representations about
+ * the suitability of this software for any purpose.  It is provided
+ * "as is" without express or implied warranty.
+ *
+ */
+
+
+#include "GMTK_LatticeNodeCPT.h"
+#include "GMTK_CPT.h"
+#include "GMTK_DiscRV.h"
+
+
+/*-
+ *-----------------------------------------------------------------------
+ * LatticeNodeCPT::LatticeNodeCPT()
+ *     default constructor
+ *
+ * Results:
+ *     no results
+ *-----------------------------------------------------------------------
+ */
+LatticeNodeCPT::LatticeNodeCPT() : CPT(di_LatticeNodeCPT) , _latticeAdt(NULL) {
+  // some values are fixed
+  // the first parent must be previous node and second one must be
+  // word transition
+  _numParents = 2;
+  cardinalities.resize(2);
+}
+
+
+/*-
+ *-----------------------------------------------------------------------
+ * LatticeNodeCPT::~LatticeNodeCPT()
+ *     default destructor
+ *
+ * Results:
+ *     no results
+ *-----------------------------------------------------------------------
+ */
+LatticeNodeCPT::~LatticeNodeCPT() {
+}
+
+
+/*-
+ *-----------------------------------------------------------------------
+ * LatticeNodeCPT::becomeAwareOfParentValuesAndIterBegin(vector< RV* >& parents, iterator &it, DiscRV* drv, logpr& p)
+ *     assign parent value and begin an iterator
+ *
+ * Results:
+ *     no results
+ * 
+ * Notes:
+ *     need more implementation on different scores
+ *-----------------------------------------------------------------------
+ */
+void LatticeNodeCPT::becomeAwareOfParentValuesAndIterBegin(vector< RV* >& parents, iterator &it, DiscRV* drv, logpr& p) {
+  // 
+  // We assume that there are either 2 or 3 parents,
+  // in both cases, 
+  //    drv is the current lattice node.
+  // In the case of 2 parents, (where time is normally obtained from 'drv', the current lattice node)
+  //      parent[0] is the previous lattice node
+  //      parent[1] is the  "word transition" (or variable that is acting like such a construct)
+  // In the case of 3 parents, 
+  //      parent[0] is the previous lattice node
+  //      parent[1] is the the "word transition" (or variable that is acting like such a construct)
+  //      parent[2] is the "time observation", namely it is a variable that is presumably observed that
+  //                keeps track of the time frame to use (rather than using the time frame of 'drv').
+  // 
+  // For simplity reason, here cardinality of lattice nodes can be
+  // bigger than number of real nodes in some particular lattice.
+  // This is because in iterable lattices, different lattices can
+  // have different number of nodes.  But in master file, we can
+  // just specify the max of those.
+
+  // initialize it to something always at least valid.
+  drv->val = 0;
+
+#if 0
+  // TODO: why is this here?
+
+  if ( RV2DRV(parents[0])->val > _latticeAdt->_end ) {
+    it.internalStatePtr = NULL;
+    p.set_to_zero();
+    return;
+  }
+#endif
+
+  if ( _latticeAdt->useTimeParent() )
+  {
+    // use time parent to check time
+
+    // first, compute lat_time, the time of the previous lattice node
+    // rounded to the closest frame. parent[0] contains the value of
+    // the previous lattice node, and we need to do a lookup in the
+    // lattce to find the actual previous lattice node to get its
+    // time.
+    unsigned lat_time = (unsigned)round(_latticeAdt->_frameRate * _latticeAdt->_latticeNodes[RV2DRV(parents[0])->val].time);
+
+    // case on the current time.
+    // Same as no time parent case, we also allow some relaxation
+    // of time.
+    if ( RV2DRV(parents[2])->val < lat_time - _latticeAdt->_frameRelax ) {
+      // then the current time is less than the previous lattice node.
+      if ( RV2DRV(parents[1])->val ) {
+	// a (word) transition is being hypothesized, but we do not allow it. Give it a
+	// zero probability.
+	it.internalStatePtr = NULL;
+	p.set_to_zero();
+      } else {
+	// a (word) transition is not being hypothesized, so
+	// we copy the previous lattice node's value (from previous frame) to drv.
+	it.internalStatePtr = NULL;
+	drv->val = RV2DRV(parents[0])->val;
+	p.set_to_one();
+      }
+    } else if ( RV2DRV(parents[2])->val > lat_time + _latticeAdt->_frameRelax ) {
+      // Then, the current time (i.e., parent[2]'s time value) is
+      // already ahead (i.e., after, later) of when a transition for
+      // the previous lattice node value may occur. We also want to
+      // force this not to happen by setting prob to zero. While
+      // you might think this might be valid, only allow jumping
+      // from the prevous lattice node when the time is exactly
+      // equal to the previous lattice nodes.
+
+      it.internalStatePtr = NULL;
+      p.set_to_zero();
+    } else {
+      // (RV2DRV(parents[2])->val == lat_time), which means that the
+      // current time is right at the point that we allow the previous
+      // lattice node to jump to the next set of possible lattice
+      // nodes.
+
+
+      if ( RV2DRV(parents[1])->val ) {
+	// iterate next lattice nodes
+	// find the out going edge
+	LatticeADT::LatticeNode &node = _latticeAdt->_latticeNodes[RV2DRV(parents[0])->val];
+
+	// if the lattice node is the end, return prob zero
+	if ( node.edges.totalNumberEntries() == 0 ) {
+	  it.internalStatePtr = NULL;
+	  p.set_to_zero();
+	  return;
+	}
+
+	// check out the out-going edges based on parent value
+	shash_map_iter<unsigned, LatticeADT::LatticeEdge>::iterator *pit = new shash_map_iter<unsigned, LatticeADT::LatticeEdge>::iterator();
+
+	// now the current node can have next transition
+	// find the correct iterators
+	node.edges.begin(*pit);
+
+	// set up the internal state for iterator
+	it.internalStatePtr = (void*)pit;
+	it.internalState = RV2DRV(parents[0])->val;
+	it.drv = drv;
+
+	drv->val = pit->key();
+
+	p = (**pit).gmtk_score;
+      } else {
+	// no word transition, copy value
+	it.internalStatePtr = NULL;
+	drv->val = RV2DRV(parents[0])->val;
+	p.set_to_one();
+      }
+    }
+  } else {
+    // case on the current frame index
+    if ( drv->frame() < _latticeAdt->_latticeNodes[RV2DRV(parents[0])->val].startFrame ) {
+      if ( RV2DRV(parents[1])->val ) {
+        // word transition not allowed
+        it.internalStatePtr = NULL;
+        p.set_to_zero();
+      } else {
+        // no word transition, just copy values from previous frame
+        it.internalStatePtr = NULL;
+        drv->val = RV2DRV(parents[0])->val;
+        p.set_to_one();
+      }
+    } else if ( drv->frame() > _latticeAdt->_latticeNodes[RV2DRV(parents[0])->val].endFrame ) {
+      // we force this cannot happen by setting prob to zero
+      it.internalStatePtr = NULL;
+      p.set_to_zero();
+    } else {
+      if ( RV2DRV(parents[1])->val ) {
+        // iterate next lattice nodes
+        // find the out going edge
+        LatticeADT::LatticeNode &node = _latticeAdt->_latticeNodes[RV2DRV(parents[0])->val];
+
+        // if the lattice node is the end, return prob zero
+        if ( node.edges.totalNumberEntries() == 0 ) {
+	  it.internalStatePtr = NULL;
+          p.set_to_zero();
+          return;
+        }
+
+        // check out the out-going edges based on parent value
+        shash_map_iter<unsigned, LatticeADT::LatticeEdge>::iterator *pit = new shash_map_iter<unsigned, LatticeADT::LatticeEdge>::iterator();
+
+        // now the current node can have next transition
+        // find the correct iterators
+        node.edges.begin(*pit);
+
+        // set up the internal state for iterator
+        it.internalStatePtr = (void*)pit;
+        it.internalState = RV2DRV(parents[0])->val;
+        it.drv = drv;
+
+        drv->val = pit->key();
+        p = (**pit).gmtk_score;
+      } else {
+        // no word transition, copy value
+        it.internalStatePtr = NULL;
+        drv->val = RV2DRV(parents[0])->val;
+        p.set_to_one();
+      }
+    }
+  }
+}
+
+
+/*-
+ *-----------------------------------------------------------------------
+ * LatticeNodeCPT::probGivenParents(vector< RV* >& parents, DiscRV* drv)
+ *     calculate the probability with known parent/child values
+ *
+ * Results:
+ *     log probability of child given the parents
+ * 
+ * Notes:
+ *     need more implementation on different scores
+ *-----------------------------------------------------------------------
+ */
+logpr LatticeNodeCPT::probGivenParents(vector< RV* >& parents, DiscRV* drv) {
+	LatticeADT::LatticeEdge* outEdge = _latticeAdt->_latticeNodes[RV2DRV(parents[0])->val].edges.find(drv->val);
+
+	if ( outEdge == NULL )
+		return logpr(0.0);
+	else
+		return outEdge->gmtk_score;
+}
+
+
+/*-
+ *-----------------------------------------------------------------------
+ * LatticeNodeCPT::next(iterator &it, logpr& p)
+ *     proceed to next iterator
+ *
+ * Results:
+ *     true if the next exists
+ * 
+ * Notes:
+ *     need more implementation on different scores
+ *-----------------------------------------------------------------------
+ */
+bool LatticeNodeCPT::next(iterator &it, logpr& p) {
+  shash_map_iter<unsigned, LatticeADT::LatticeEdge>::iterator* pit = (shash_map_iter<unsigned, LatticeADT::LatticeEdge>::iterator*) it.internalStatePtr;
+
+  // check whether pit is null
+  if ( pit == NULL ) {
+    p.set_to_zero();
+    return false;
+  }
+
+  // No need to check the time constrains in the lattice
+  // transitions.  The reason is once the time passed
+  // checking in BeginIterate, it already satisfies the constrains.
+
+  // find the next available in the tree
+  if ( pit->next() ) {
+    // set up the values
+    it.drv->val = pit->key();
+    p = (**pit).gmtk_score;
+
+    return true;
+  } else {
+    // we didn't find anything satisfying the frame constrain
+    delete pit;
+    it.internalStatePtr = NULL;
+    p.set_to_zero();
+    return false;
+  }
+}
+
+
+/*-
+ *-----------------------------------------------------------------------
+ * LatticeNodeCPT::setLatticeADT(const LatticeADT &latticeAdt)
+ *     set the lattice ADT to be used for this CPT
+ *
+ * Results:
+ *     none.
+ *-----------------------------------------------------------------------
+ */
+void LatticeNodeCPT::setLatticeADT(const LatticeADT &latticeAdt) {
+  _latticeAdt = &latticeAdt;
+
+  // in addtiont to setting lattice ADT, also need to set
+  // up the number of parents and cardinalities of the parents.
+
+  // typically, cardinalities comes from structure file or master
+  // file.  But in this case, we hope to support iterable lattice
+  // CPTs which will have different number of lattice nodes for
+  // each one.
+
+  // check whether time is also used as parent
+  if ( _latticeAdt->useTimeParent() ) {
+    _numParents = 3;
+    cardinalities.resize(3);
+    cardinalities[2] = _latticeAdt->_timeCardinality;
+  } else {
+    _numParents = 2;
+    cardinalities.resize(2);
+  }
+
+  // first parent is noade
+  _card = cardinalities[0] = _latticeAdt->_nodeCardinality;
+  // second parent is word transition
+  cardinalities[1] = 2;
+}
---
+++ tksrc/GMTK_RVInfo.h
@@ -0,0 +1,303 @@
+/*
+ * GMTK_RVInfo.h
+ * A class that keeps all the information about a random
+ * variable that is unique to its location in the structure
+ * file (i.e., this is a random variable "class", and the information 
+ * kept here is common to *all* instantiations of a random variable).
+ *
+ *
+ * Written by Jeff Bilmes <bilmes@ee.washington.edu>
+ *
+ * Copyright (c) 2001, < fill in later >
+ *
+ * Permission to use, copy, modify, and distribute this
+ * software and its documentation for any non-commercial purpose
+ * and without fee is hereby granted, provided that the above copyright
+ * notice appears in all copies.  The University of Washington,
+ * Seattle make no representations about the suitability of this software
+ * for any purpose. It is provided "as is" without express or implied warranty.
+ *
+ *
+ * $Header$
+ *
+ */
+
+#ifndef GMTK_RVINFO_H
+#define GMTK_RVINFO_H
+
+#include <vector>
+#include <string>
+#include <map>
+
+#include <stdio.h>
+#include <stdlib.h>
+
+class RV;
+class DiscRV;
+class ContRV;
+class FileParser;
+class GMTemplate;
+class Partition;
+class BoundaryTriangulate;
+class NameCollection;
+
+#include "GMTK_CPT.h"
+#include "GMTK_MixtureCommon.h"
+
+class RVInfo {
+  friend class FileParser;
+  friend class GMTemplate;
+  friend class Partition;
+  friend class BoundaryTriangulate;
+  friend class JunctionTree;
+  friend class StructPage;
+  friend class VizNode;
+  friend class GraphicalModel;
+
+  friend class RV;
+  friend class DiscRV;
+  friend class ObsDiscRV;
+  friend class Sw_ObsDiscRV;
+  friend class ScPnSh_ObsDiscRV;
+  friend class ScPnSh_Sw_ObsDiscRV;
+  friend class HidDiscRV;
+  friend class Sw_HidDiscRV;
+  friend class ScPnSh_HidDiscRV;
+  friend class ScPnSh_Sw_HidDiscRV;
+  friend class ObsContRV;
+  friend class Sw_ObsContRV;
+  friend class ScPnSh_ObsContRV;
+  friend class ScPnSh_Sw_ObsContRV;
+  friend class ScPnShRV;
+  friend class ContRV;
+  friend class SwRV;
+  friend class SwDiscRV;
+
+public:
+  ///////////////////////////////////////////////////
+  // TODO: change the name of this from rvParent to something like rvID (since
+  // this isn't nec. a parent, but a general ID of a random variable).
+  typedef pair<string,int> rvParent;
+
+  ////////////////////////////////////////////////////////////
+  // define a bunch of types that are used in RVs
+  enum Type { t_discrete, t_continuous, t_unknown };
+
+  enum Disposition { d_hidden, d_observed, d_unknown };
+
+  struct WeightInfo {
+
+    // Given a probability p, the weight info modifies the
+    // probability according to the following formula:
+    //
+    //        penalty*p^scale+shift
+    //
+    // each of penalty, scale, and shift might be an immediate value
+    // or might come from the observation file at a frame given by the
+    // child class.
+
+    struct WeightItem {
+      enum wtEnum { wt_NoWeight, wt_Constant, wt_Observation };
+      wtEnum wt_Status;
+      // the values of scale,penalty, or shift depending on the
+      // particular case. Note that the value stored is the
+      // natural_log(value), where value is scale, penalty, or shift
+      // above.
+      double weight_value;
+      unsigned firstFeatureElement;
+      unsigned lastFeatureElement;
+      void clear() { wt_Status = wt_NoWeight; }
+    };
+
+    WeightItem penalty;
+    WeightItem scale;
+    WeightItem shift;
+    WeightInfo() { clear(); }
+    void clear() { penalty.clear(); scale.clear(); shift.clear(); }
+
+  };
+
+  struct FeatureRange {
+    enum frEnum { fr_UnFilled, fr_Range, fr_FirstIsValue,
+		  fr_FrameNumIsValue, fr_EmarfNumIsValue,
+		  fr_NumFramesIsValue,
+		  fr_SegmentNumIsValue, fr_NumSegmentsIsValue
+    };
+    frEnum filled;
+    unsigned firstFeatureElement;
+    unsigned lastFeatureElement;
+    FeatureRange() { filled = fr_UnFilled; }
+    void clear() { filled = fr_UnFilled; }
+  };
+
+  struct ListIndex {
+    enum ListIndexType { li_String, li_Index, li_Unknown } liType;
+    // if this is an integer index, this is used.
+    unsigned intIndex;
+    // Otherwise, if this is an string index, this is used.
+    string nameIndex;
+    // the following is for FNGramCPT only
+    // when all FNGramCPT use all parents in each switching branch
+    // the size will be zero. Otherwise the size will equal to
+    // actual number of parents
+    vector <unsigned> fnparents;
+    // in cases where this is a reference to a DT that
+    // is supposed to reference via a collection, here
+    // is the name of the collection.
+    string collectionName;
+    void clear() { liType = li_Unknown; }
+  };
+
+private:
+
+  // A hint given to the triangulation algorithm.
+  // this is something that can be specified in the structure
+  // file and can be used as a selector during triangulation.
+  float eliminationOrderHint;
+
+  // The position of this variable in the structure file
+  // (i.e., this variable indicates the order in which
+  // this variable is encountered when parsing). It is
+  // used as a possible triangulation hint. Value -1
+  // means uninitialized.
+  int variablePositionInStrFile;
+
+  ///////////////////////////////////////////////////////////
+  // data associated with a RV
+
+  // TODO: remove some of the friends above and make public interfaces to
+  //        these as read only.
+  // the frame where it was defined (so is part of name really)
+  unsigned frame;
+  // line number of file where this RV was first declared
+  unsigned fileLineNumber;
+  // file name where this r.v. is first defined.
+  string rvFileName;
+  // rv's name
+  string name;
+
+  // fields from type
+  Type rvType;
+  Disposition rvDisp;
+  unsigned rvCard;
+  // if it is an observed variable, it must have a feature range.
+  FeatureRange rvFeatureRange;
+  // a status flag that tells if the variable is deterministic or not.
+  // This has to be computed once the details of the RV are filled in.
+  bool isDeterministic;
+  // same thing for sparse
+  bool isSparse;
+
+  // switching parents and weight stuff
+  vector< rvParent > switchingParents;
+
+  ListIndex switchMapping;
+
+  // conditional parents stuff
+  vector<vector< rvParent > > conditionalParents;
+  // if discrete, then the list if discrete implementations
+  vector< CPT::DiscreteImplementaton > discImplementations;
+  // if continuous, the list of continuous implementations
+  vector< MixtureCommon::ContinuousImplementation > contImplementations;
+  // in either case, a low-level parameter index
+  vector< ListIndex > listIndices;
+
+  // (possibly switching) weight information.  Note that either
+  // (rvWeightInfo.size() == 1), or (rvWeightInfo.size() ==
+  // conditionalParents.size()).
+  vector <WeightInfo> rvWeightInfo;
+
+
+  // if discrete, then a possible symbol table to get
+  // names of integer values from (which is a named collection object).
+  string symbolTableCollectionName;
+  NameCollection* symbolTable;
+
+
+  /////////////////////////////////////////////////////////
+  // An actual pointer to the RV once we instantiate it.  Note that
+  // in general, there is a many-to-one mapping from random variable
+  // instances to RVInfo instances.  This pointer here is used
+  // only by the parser to create the very first set of random
+  // variables that correspond exactly to the structure file. If you
+  // will, you can consider rv here to be the Adam&Eve random
+  // variable since all instances of this are going to be clones of
+  // 'rv' (or clones of clones of 'rv', etc.).
+  RV* rv;
+
+public:
+
+  /////////////////////////////////////////////////////////
+  // constructor
+  RVInfo() : rv(NULL) { clear(); }
+  // destructor
+  ~RVInfo() { clear(); }
+
+  // copy constructor
+  RVInfo(const RVInfo&v) {
+    frame = v.frame;
+    fileLineNumber = v.fileLineNumber;
+    rvFileName = v.rvFileName;
+    name = v.name;
+    rvType = v.rvType;
+    rvDisp = v.rvDisp;
+    rvCard = v.rvCard;
+    eliminationOrderHint = v.eliminationOrderHint;
+    variablePositionInStrFile = v.variablePositionInStrFile;
+    rvFeatureRange = v.rvFeatureRange;
+    rvWeightInfo = v.rvWeightInfo;
+    switchingParents = v.switchingParents;
+    switchMapping = v.switchMapping;
+    conditionalParents = v.conditionalParents;
+    discImplementations = v.discImplementations;
+    contImplementations = v.contImplementations;
+    listIndices = v.listIndices;
+    rv = v.rv;
+    isDeterministic = v.isDeterministic;
+    isSparse = v.isSparse;
+    symbolTableCollectionName = v.symbolTableCollectionName;
+    symbolTable = v.symbolTable;
+  }
+
+  // clear out the current RV structure when we
+  // are parsing and encounter a new RV.
+  void clear();
+
+  bool computeAndReturnDeterministicStatus() {
+    isDeterministic = false;
+    for (unsigned i=0;i<discImplementations.size();i++) {
+      if ((discImplementations[i] != CPT::di_MTCPT) 
+	  &&
+	  (discImplementations[i] != CPT::di_LatticeEdgeCPT))
+	return false; 
+    }
+    isDeterministic = true;
+    return true;
+  }
+
+  bool deterministic() { return isDeterministic; }
+
+  bool computeAndReturnSparseStatus() {
+    isSparse = false;
+    for (unsigned i=0;i<discImplementations.size();i++) {
+      if ((discImplementations[i] != CPT::di_MTCPT)
+	  &&
+	  (discImplementations[i] != CPT::di_MSCPT)
+	  &&
+	  (discImplementations[i] != CPT::di_LatticeEdgeCPT))
+	return false; 
+    }
+    isSparse = true;
+    return true;
+  }
+
+  bool sparse() { return isSparse; }
+
+  static rvParent parseRVParent(const char* const);
+
+};
+
+
+
+
+#endif
