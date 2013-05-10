580555a9d73262a96b027a353e90da68d99856c2
Richard Rogers <rprogers@uw.edu>
2013-05-09 22:17:12 -0700

    merged in trunk

---
+++ IEEEFloatingpoint/ieeeFPsetup_osx.h
@@ -0,0 +1,19 @@
+//
+//
+// Copyright (C) 2007 Jeff Bilmes
+// Licensed under the Open Software License version 3.0
+//
+//
+// $Header$
+// Written by: Jeff Bilmes
+//             bilmes@ee.washington.edu
+
+
+#ifndef IEEE_FP_SETUP_H
+#define IEEE_FP_SETUP_H
+
+#include <float.h>
+
+void ieeeFPsetup();
+
+#endif
