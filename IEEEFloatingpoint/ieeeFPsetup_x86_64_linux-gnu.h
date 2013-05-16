862a8234c1cccac1b64437192d335a79971ea41b
Richard Rogers <rprogers@uw.edu>
2013-05-16 01:24:50 -0700

    merged {{{ticket406}}} into trunk; fixes #406 and #421

---
+++ IEEEFloatingpoint/ieeeFPsetup_x86_64_linux-gnu.h
@@ -0,0 +1,26 @@
+/*
+**
+**
+** Copyright (C) 2010 Jeff Bilmes
+** Licensed under the Open Software License version 3.0
+**
+** This code will set up the FPU on x86 archiectures to trap with
+** SIGFPEs when an inf or nan occurs (or some other fp exception depending
+** on what is uncommented below).
+**
+** The code is very machine specific to Linux (and probably to a particular
+** version of Linux).
+**
+** Jeff Bilmes
+** <bilmes@ee.washington.edu> 
+**
+**
+*/
+
+#ifndef IEEESETUP_H
+#define IEEESETUP_H
+
+extern void ieeeFPsetup();
+
+#endif
+
