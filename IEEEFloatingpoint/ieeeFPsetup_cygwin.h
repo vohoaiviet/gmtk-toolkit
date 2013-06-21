4edad98fb630cf1de6006771bff0f5f74afd04b6
Richard Rogers <rprogers@uw.edu>
2013-06-21 11:41:51 -0700

    skip slow staka models, decompress as needed

---
+++ IEEEFloatingpoint/ieeeFPsetup_cygwin.h
@@ -0,0 +1,20 @@
+//
+//
+//
+// Copyright (C) 2002 Jeff Bilmes
+// Licensed under the Open Software License version 3.0
+//
+// $Header$
+// Written by: Jeff Bilmes
+//             bilmes@ee.washington.edu
+
+
+#ifndef IEEE_FP_SETUP_H
+#define IEEE_FP_SETUP_H
+
+#include <float.h>
+#include <ieeefp.h>
+
+void ieeeFPsetup();
+
+#endif
