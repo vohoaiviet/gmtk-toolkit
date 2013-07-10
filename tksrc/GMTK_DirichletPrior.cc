41db2b6b368c610eebad9558f12734909919fc7b
Richard Rogers <rprogers@uw.edu>
2013-07-09 23:09:25 -0700

    created working branch; refs #435

---
+++ tksrc/GMTK_DirichletPrior.cc
@@ -0,0 +1,42 @@
+/*-
+ * GMTK_DirichletPrior.cc
+ *
+ * Written by Jeff Bilmes <bilmes@ee.washington.edu>
+ *
+ * Copyright (C) 2001 Jeff Bilmes
+ * Licensed under the Open Software License version 3.0
+ *
+ *
+ */
+
+
+
+#include <math.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <errno.h>
+#include <string.h>
+#include <float.h>
+#include <assert.h>
+
+#include "general.h"
+#include "error.h"
+
+#include "GMTK_DirichletPrior.h"
+
+#if HAVE_CONFIG_H
+#include <config.h>
+#endif
+#if HAVE_HG_H
+#include "hgstamp.h"
+#endif
+VCID(HGID)
+
+
+/*
+ * Static Variable definitions 
+*/
+
+const char * DirichletPrior::DirichletConstStr = "DirichletConst";
+const char * DirichletPrior::DirichletTableStr = "DirichletTable";
+
