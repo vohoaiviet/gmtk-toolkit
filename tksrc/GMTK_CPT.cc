38f6c4ec682c9390673c5368d8ca8c912470c142
Richard Rogers <rprogers@uw.edu>
2013-08-06 10:23:44 -0700

    initial work on Viterbi output to observation file; refs #435

---
+++ tksrc/GMTK_CPT.cc
@@ -0,0 +1,156 @@
+/*-
+ * GMTK_CPT.cc
+ *     Trainable (with say EM) CPT
+ *
+ * Written by Jeff Bilmes <bilmes@ee.washington.edu>
+ *
+ * Copyright (C) 2001 Jeff Bilmes
+ * Licensed under the Open Software License version 3.0
+ *
+ *
+ */
+
+
+
+#include <math.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <errno.h>
+#include <string.h>
+#include <float.h>
+#include <assert.h>
+
+#include "general.h"
+#include "error.h"
+
+#include "GMTK_CPT.h"
+
+
+#if HAVE_CONFIG_H
+#include <config.h>
+#endif
+#if HAVE_HG_H
+#include "hgstamp.h"
+#endif
+VCID(HGID)
+
+
+////////////////////////////////////////////////////////////////////
+//        Static Data
+////////////////////////////////////////////////////////////////////
+
+unsigned CPT::warningNumParents = 50;
+
+/////////////////////////////////////////////////////////////////
+// threshold to ensure input data sums to approximately unity
+double CPT::normalizationThreshold = 1e-2;
+
+////////////////////////////////////////////////////////////////////
+//        General create, read, destroy routines 
+////////////////////////////////////////////////////////////////////
+
+
+
+////////////////////////////////////////////////////////////////////
+//        Misc Support
+////////////////////////////////////////////////////////////////////
+
+
+/*-
+ *-----------------------------------------------------------------------
+ * CPT::setNumParents()
+ *      Just sets the number of parents and resizes arrays as appropriate.
+ *
+ * Results:
+ *      no results.
+ *
+ * Side Effects:
+ *      Will change internal arrays of this object.
+ *
+ *-----------------------------------------------------------------------
+ */
+void CPT::setNumParents(const unsigned _nParents)
+{
+
+  if (_nParents < 0) 
+    error("CPT: setNumParents, trying to use negative (%d) num parents.",
+	  _nParents);
+
+  _numParents = _nParents;
+  cardinalities.resize(_numParents);
+
+}
+
+
+
+/*-
+ *-----------------------------------------------------------------------
+ * CPT::setNumCardinality(var,card)
+ *      sets the cardinality of var to card
+ *      to set self, var == _numParents
+ *
+ * Results:
+ *      no results.
+ *
+ * Side Effects:
+ *      Will change internal array content of this object.
+ *
+ *-----------------------------------------------------------------------
+ */
+void CPT::setNumCardinality(const unsigned var, const int card)
+{
+
+  if (var < 0)
+    error("CPT: setNumCardinality, trying to use negative (%d) var.",
+	  var);
+  if (var > _numParents) 
+    error("CPT: setNumCardinality, trying to use illegal (%d) var.",
+	  var);
+  if (card <= 0)
+    error("CPT: setNumCardinality, trying to use illegal (%d) card.",
+	  card);
+
+  // assertion should be satisifed by the way that cardinalities
+  // is allocated allong with setting num parents.
+  if ( var == _numParents ) 
+    _card = card;
+  else { 
+    assert ( var < cardinalities.size() );
+    cardinalities[var] = card;
+  }
+
+}
+
+
+
+/*-
+ *-----------------------------------------------------------------------
+ * Function
+ *      compareCardinalities: compare the cardinalities of this CPT with that of an other. REturn
+ *      true if they are equal false otherwise.
+ *
+ * Results:
+ *      returns true if cards are equal.
+ *
+ * Side Effects:
+ *      none
+ *
+ *-----------------------------------------------------------------------
+ */
+bool 
+CPT::compareCardinalities(CPT& cpt)
+{
+  if (cardinalities.size() != cpt.cardinalities.size())
+    return false;
+
+  for (unsigned int i=0;i<cardinalities.size();i++) {
+    if (cardinalities[i] != cpt.cardinalities[i])
+      return false;
+  }
+  if (_card != cpt._card)
+    return false;
+
+  return true;
+}
+
+
---
+++ tksrc/GMTK_ScPnSh_Sw_ObsDiscRV.h
@@ -0,0 +1,87 @@
+/*
+ * GMTK_ScPnSh_Sw_ObsDiscRV.h
+ *
+ *  Scale/Penalty/Shift Switching Hidden Discrete Random Variables.
+ *
+ * Written by Jeff Bilmes <bilmes@ee.washington.edu>
+ *  $Header$
+ *
+ * Copyright (C) 2001 Jeff Bilmes
+ * Licensed under the Open Software License version 3.0
+ *
+ *
+ *
+ *
+ */
+
+#ifndef GMTK_SC_PN_SH_SW_OBS_DISC_RV_H
+#define GMTK_SC_PN_SH_SW_OBS_DISC_RV_H
+
+#include <vector>
+
+#include "GMTK_Sw_ObsDiscRV.h"
+#include "GMTK_ScPnShRV.h"
+
+class ScPnSh_Sw_ObsDiscRV : public Sw_ObsDiscRV, public ScPnShRV {
+  friend class FileParser;
+
+protected:
+
+public:
+
+  /////////////////////////////////////////////////////////////////////////
+  // constructor: Initialize with the variable type.  The default
+  // timeIndex value of ~0x0 indicates a static network.  Discrete
+  // nodes must be specified with their cardinalities.
+  ScPnSh_Sw_ObsDiscRV(RVInfo& _rv_info,
+		   unsigned _timeFrame = ~0x0,
+		   unsigned _cardinality = 0)
+    : Sw_ObsDiscRV(_rv_info,_timeFrame,_cardinality)
+  {
+  }
+
+  void printSelf(FILE *f,bool nl=true) {
+    printNameFrameValue(f,false);
+    ScPnShRV::printSelf(rv_info.rvWeightInfo[0],f,false);
+    fprintf(f,"switching observed discrete cardinality = %d%s",cardinality,nls(nl));
+  }
+
+  void printSelfVerbose(FILE *f) {
+    fprintf(f,"Switching Observed Discrete Random variable:\n");
+    printNameFrameValue(f,true);
+    fprintf(f,"From line %d in file %s\n",rv_info.fileLineNumber,rv_info.rvFileName.c_str());
+    fprintf(f,"RV has cardinality = %d\n",cardinality);
+    ScPnShRV::printSelfVerbose(rv_info.rvWeightInfo[0],f);
+  }
+  
+  void begin(logpr& p) {
+    Sw_ObsDiscRV::begin(p);
+    if (rv_info.rvWeightInfo.size() > 1) 
+      modifyProbability(p,rv_info.rvWeightInfo[cachedSwitchingState],this);
+    else 
+      modifyProbability(p,rv_info.rvWeightInfo[0],this);
+    return;
+  }
+
+  void probGivenParents(logpr& p) {
+    setCurrentConditionalParents(this);
+    curCPT = conditionalCPTs[cachedSwitchingState];
+    p = curCPT->probGivenParents(*curConditionalParents,this);
+    if (rv_info.rvWeightInfo.size() > 1) 
+      modifyProbability(p,rv_info.rvWeightInfo[cachedSwitchingState],this);
+    else 
+      modifyProbability(p,rv_info.rvWeightInfo[0],this);
+  }
+
+  ScPnSh_Sw_ObsDiscRV* cloneRVShell() {
+    return (ScPnSh_Sw_ObsDiscRV*)Sw_ObsDiscRV::cloneRVShell();
+  }
+  ScPnSh_Sw_ObsDiscRV* create() {
+    ScPnSh_Sw_ObsDiscRV*rv = new ScPnSh_Sw_ObsDiscRV(rv_info,frame(),cardinality);
+    return rv;
+  }
+
+};
+
+
+#endif
